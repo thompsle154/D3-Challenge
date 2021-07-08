6bb0ae055ecfbf1aad61c310683cc1e7e966313f
Leanne Thompson <78496051+thompsle154@users.noreply.github.com>
2021-07-08 17:35:12 -0600

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,7 @@ d3.csv("./assets/data/data.csv").then(function (healthdata) {
         data.healthcare = +data.healthcare;
     });
 
-    // Step 2: Create scale functions
+    // Create scale functions
 
     var xLinearScale = d3.scaleLinear()
         .domain([0, d3.max(healthdata, d => d.poverty)])
@@ -34,4 +34,84 @@ d3.csv("./assets/data/data.csv").then(function (healthdata) {
     var yLinearScale = d3.scaleLinear()
         .domain([0, d3.max(healthdata, d => d.healthcare)])
         .range([height, 0]);
+ // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .classed("purple", true)
+        .call(leftAxis);
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(healthdata)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "15")
+        .attr("fill", "#00BFFF")
+        .attr("opacity", ".5")
+        ;
+    // This is the abbreviation for states in the circles
+    var textGroup = chartGroup.selectAll("text.stabber")
+        .data(healthdata)
+        .enter()
+        .append("text")
+        .attr("class", "stabbr")
+        .text(d => d.abbr)
+        .attr("dx", d => xLinearScale(d.poverty))
+        .attr("dy", d => yLinearScale(d.healthcare))
+        .style("text-anchor", "middle")
+        .attr("stroke", "black");
+
+    // Step 1: CREATE tooltips, assign it a class and Append a div to the body
+    // When you click on the circles, this code goes into action.
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function (d) {
+            return (`${d.state}<br>Poverty: ${d.poverty}<br>healthcare: ${d.healthcare}`);
+
+        })
+        .style("background-color", "#00DDFF");
+
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function (data) {
+        toolTip.show(data);
+    })
+        // onmouseout event
+        .on("mouseout", function (data, index) {
+            toolTip.hide(data);
+        });
+
+    // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("stroke", "#CC33FF") //y text
+        .text("In Poverty (%)")
+        ;
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("stroke", "red") //X text
+        .text("Lacks Healthcare (%)");
+}).catch(function (error) {
+    console.log(error);
+});
+
 
